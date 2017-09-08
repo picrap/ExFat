4e54fdc567c0baa9b74d47ca8b54288429bc80a1
Pascal Craponne <pascal@craponne.fr>
2017-09-09 00:41:02 +0200

    Placed empty files instead of generated

--- ExFat.Core/Properties/ProductInfo.cs
+++ ExFat.Core/Properties/ProductInfo.cs
@@ -1,15 +1,1 @@
-﻿
-// This is ExFat, an exFAT accessor written in pure C#
-// Released under MIT license
-// https://github.com/picrap/ExFat
-
-using System.Reflection;
-
-[assembly: AssemblyCompany("picrap")]
-[assembly: AssemblyProduct("ExFat")]
-[assembly: AssemblyCopyright("MIT license http://opensource.org/licenses/mit-license.php")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+﻿#error TransformOnBuild failted its mission :'(
